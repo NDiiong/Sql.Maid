6fa548817b975cbbd0d3468f9df390418d689b1f
duong.nguyen <nb.duong@outlook.com>
2022-06-16 17:18:56 +0700

    Add ISqlServiceProvider

---
+++ src/Sqlserver-maid/Services/SqlServiceProvider/ISqlServiceProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using System;
+
+namespace Sqlserver.maid.Services.SqlTextView
+{
+    internal interface ISqlServiceProvider
+    {
+        IWpfTextView GetSqlWpfTextView(IServiceProvider serviceProvider);
+    }
+}
---
+++ src/Sqlserver-maid/Services/SqlServiceProvider/SqlServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+
+namespace Sqlserver.maid.Services.SqlTextView
+{
+    internal class SqlServiceProvider : ISqlServiceProvider
+    {
+        public IWpfTextView GetSqlWpfTextView(IServiceProvider serviceProvider)
+        {
+            IVsTextManager vsTextManager = serviceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+            IComponentModel componentModel = serviceProvider.GetService(typeof(SComponentModel)) as IComponentModel;
+            IVsEditorAdaptersFactoryService vsEditorAdaptersFactoryService = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+            vsTextManager.GetActiveView(1, null, out IVsTextView ppView);
+            return vsEditorAdaptersFactoryService.GetWpfTextView(ppView);
+        }
+    }
+}
